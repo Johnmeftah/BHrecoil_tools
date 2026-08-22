1b61807dfd7d13f43d3e9931e7141ced666dcc32
John Meftah <70301036+Johnmeftah@users.noreply.github.com>
2026-08-21 20:34:35 -0400

    Delete changa_analysis_guide/changa_analysis_guide/week_1 directory

--- changa_analysis_guide/changa_analysis_guide/week_1/day_1/notes.md
+++
@@ -1,1 +0,0 @@
-Day 1: loading a Changa snapshot, checking if it was loaded correctly by checking it's type, and counting the number of particles in that snapshot. 
--- changa_analysis_guide/changa_analysis_guide/week_1/day_1/real_data_practice.py
+++
@@ -1,7 +0,0 @@
-import pynbody as pn 
-
-# loading the snapshot 
-snapshot = pn.load("/home/jmeftah/python/sample_data/JM_recoil0/pioneer50h243.1536gst1bwK1BH.000128")
-
-print(f'Snapshot type: {type(snapshot)}') # checking the type of snapshot
-print(f'Total number of particles in this snapshot: {len(snapshot)}') 
